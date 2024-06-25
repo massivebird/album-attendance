398512ed918efbef0ab990bc86225f431cee134e
massivebird <gdrakemail@gmail.com>
2024-06-25 18:58:22 -0400

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::ReadDir;
+
 use clap::Arg;
 
 fn main() {
@@ -16,21 +18,37 @@ fn main() {
         std::fs::read_dir(path).expect("Failed to open specified directory")
     };
 
-    for file in root.filter_map(Result::ok) {
+    for file in root
+        .filter_map(Result::ok)
+        .filter(|d| d.file_type().unwrap().is_dir())
+    {
         // ignore non-directories
-        let artist = match file.file_type() {
-            Ok(d) if d.is_dir() => std::fs::read_dir(file.path()).unwrap(),
+        let artist = std::fs::read_dir(file.path()).unwrap();
+
+        for album in artist
+            .filter_map(Result::ok)
+            .filter(|d| d.file_type().unwrap().is_dir())
+            .filter_map(|d| Some(std::fs::read_dir(d.path())))
+            .filter_map(Result::ok)
+        {
+            let track_numbers = compile_track_numbers(album);
+        }
+    }
+}
+
+fn compile_track_numbers(album: ReadDir) -> Vec<u32> {
+    let mut track_numbers: Vec<u32> = Vec::new();
+
+    for file in album.filter_map(Result::ok) {
+        // ignore non-musics
+        let track_name = match file.path().extension() {
+            Some(e) if e == "mp3" || e == "flac" => file.file_name(),
             _ => continue,
         };
 
-        for file in artist.filter_map(Result::ok) {
-            // ignore non-directories
-            let album = match file.file_type() {
-                Ok(d) if d.is_dir() => std::fs::read_dir(file.path()).unwrap(),
-                _ => continue,
-            };
-
-            println!("Album: {album:?}")
-        }
+        let track_number = track_name.to_string_lossy()[..2].parse::<u32>().unwrap();
+        track_numbers.push(track_number);
     }
+
+    track_numbers
 }
